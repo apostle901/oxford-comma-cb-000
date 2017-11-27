29c85f3e3e500fdce970fe3522b06e94304794ee
Jawann <jawann@gmail.com>
2017-11-27 02:16:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  return "#{array}" if array.size == 1
+  return array if array.size == 1
   return "#{array.first} and #{array.last}" if array.size == 2
   holder = array[0...-1].join(", ")
   holder << "and #{array.last}"
